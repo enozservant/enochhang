b21437dc9b435b4d08628d1017840a4e105ddc4d
Enoch Hang <enochhang@gmail.com>
2019-05-17 22:00:00 -0700

    Finished down and up

--- gitScript.js
+++ gitScript.js
@@ -3,8 +3,10 @@ var offsetY = 0, startY = null;
 var body = document.body;
 var cube = document.getElementById("tridiv");
 var cube_resume = document.getElementById("cube");
+var backUP = document.getElementById("backUP");
 cube.addEventListener("mousedown", function(event) {
-    cube_resume.style.webkitAnimationPlayState = 'paused';
+    // cube_resume.style.webkitAnimationPlayState = 'paused';
+    cube_resume.style.animation = "";
     startX = event.pageX - offsetX;
     startY = event.pageY - offsetY;
 });
@@ -19,15 +21,40 @@ cube.addEventListener("mousemove", function(event) {
         cube_resume.style['-webkit-transform'] = 'rotateY(-'+offsetX+'deg) rotateX(-'+offsetY+'deg)';
     }
 });
+backUP.addEventListener("click", function(event) {
+    smoothScroll("tridiv");
+})
 window.addEventListener("scroll", function(event) {
     //Bring Element to the bottom right of the screen fixed.
     // cube.style.top = "90%";
     // cube.style.position =
+    var first_section = document.getElementById("Experience");
+    if(window.scrollY >= first_section.offsetTop) {
+        backUP.style.display = "block";
+    } else {
+        backUP.style.display = "none";
+    }
 });
 window.addEventListener("load", function(){
     smoothScroll("tridiv");
 });
 
+var faces = document.getElementsByClassName("face");
+for(var i = 0; i < faces.length; i++) {
+    faces[i].addEventListener("click", function(event){
+        var toElement = this.childNodes[0].innerHTML;
+        smoothScroll(toElement);
+    });
+}
+
+/*
+<div class="face ft"><p class="face-text">Experience</p></div>
+<div class="face bk"><p class="face-text">Other Works</p></div>
+<div class="face rt"><p class="face-text">Education</p></div>
+<div class="face lt"><p class="face-text">Goals</p></div>
+<div class="face bm"><p class="face-text">Projects</p></div>
+<div class="face tp"><p class="face-text">Contact</p></div>
+*/
 //Used to create a corner cube
 // var extra_cube = document.getElementById("extra");
 // var cube2 = cube.cloneNode([true]);
